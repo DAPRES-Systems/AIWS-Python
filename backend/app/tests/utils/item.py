f6eb182af5676b3b9e41ff1d00a298dcb659f75c
Daniel <mine6626@gmail.com>
2024-10-14 16:53:33 +0200

    code cleanup

--- backend/app/tests/utils/item.py
+++ backend/app/tests/utils/item.py
@@ -13,4 +13,4 @@ def create_random_item(db: Session) -> Item:
     title = random_lower_string()
     description = random_lower_string()
     item_in = ItemCreate(title=title, description=description)
-    return crud.create_item(session=db, item_in=item_in, owner_id=owner_id)
+    return create_item(session=db, item_in=item_in, owner_id=owner_id)
