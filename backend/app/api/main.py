1e2c7e15dc75d7b357beac1d7becfda3803fd626
Daniel <mine6626@gmail.com>
2024-10-14 03:37:18 +0200

    Revert "Merge branch 'main' into item-properties"
    This reverts commit 63a9ddb7ed4e43e990c9a069d49e73fbc54f7735, reversing
    changes made to a4fde4eb8be3b71891e4b5ee85edf6bdaa788402.

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,16 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.routes import login, users, utils
-from app.api.api_v1.endpoints import items, locations, vehicles
-# Haupt-Router der API
-api_router = APIRouter()
+from app.api.routes import items, login, users, utils
 
-# Bestehende Router
+api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
-
-# Neue Router mit den gewünschten Prefixes
-api_router.include_router(items.router, prefix="/i", tags=["items"])
-api_router.include_router(locations.router, prefix="/l", tags=["locations"])
-api_router.include_router(vehicles.router, prefix="/v", tags=["vehicles"])
+api_router.include_router(items.router, prefix="/items", tags=["items"])
